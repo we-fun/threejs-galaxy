5de4431571d9573c9ca5028fb756a898668a206c
fritx <uxfritz@163.com>
2015-08-07 00:22:16 +0800

    part_02 进行中..

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# galaxy
+# threejs-galaxy
 我是如何用three.js创造一个宇宙的？
 
-- [part_01: 万有引力+碰撞+奇怪的向心力](http://fritx.github.io/galaxy/part_01/)
+- part_02: 皮肤+自转+公转匹配(进行中..)
+
+  <a href="http://fritx.github.io/threejs-galaxy/part_02/"><img width="300" src="https://raw.githubusercontent.com/fritx/threejs-galaxy/gh-pages/part_02/screenshot.png"></a>
+
+- part_01: 万有引力+碰撞+奇怪的向心力
+
+  <a href="http://fritx.github.io/threejs-galaxy/part_01/"><img width="300" src="https://raw.githubusercontent.com/fritx/threejs-galaxy/gh-pages/part_01/screenshot.png"></a>
--- part_02/main.js
+++ part_02/main.js
@@ -8,25 +8,21 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var G = 6.67 * Math.pow(10, -11) // 引力常量
-//var _R = Math.pow(10, 3) // 距离级数
-// 取消距离级数 即级数恒为1
-var _M = Math.pow(10, 11) // 质量级数
 
+// 向心力 GmM/r²=F=mv²/r
+// v=√(GM/r)
 var balls = [
-  createBall(1, 2, [-5, 0, 0], [0, 0, 0], 0x00ff00),
-  createBall(2, 1.5, [0, -5, 0], [0, 0, 0], 0xff0000),
-  createBall(1, 1, [0, 0, -5], [0, 0, 1], 0x0000ff),
-  createBall(1, 1, [5, 5, 0], [0, -2.4, 0], 0xff00ff)
+  createBall(1, 5, [0, 0, 60], [Math.sqrt(G*1e15/60), 0, 0], [0, 8e-2, 12e-2], 'bedrock'),
+  createBall(1e15, 20, [0, 0, 0], [0, 0, 0], [0, 4e-2, -2e-2], 'netherrack')
 ]
 
 balls.forEach(function(ball){
   scene.add(ball)
 })
 
-camera.position.y = 7;
-camera.position.z = 30;
+camera.position.set(40, 40, 240);
 
-var update = function () {
+function update(){
   requestAnimationFrame(update);
   calc()
   renderer.render(scene, camera);
@@ -37,15 +33,18 @@ update();
 
 
 function calc(){
-  var dt = clock.getDelta() // 时间差量
-  // 设定dt最大值 解决浏览窗口切换导致的dt暴增
-  dt = Math.min(dt, 0.06)
   // 根据距离质量计算受力/加速度
   //balls = shuffle(balls) // 打乱先后顺序
   balls.forEach(function(b){
-    //b.a0 = b.a.clone()
+    b.v0 = b.v.clone()
+    b.a0 = b.a.clone()
     b.a.setLength(0)
-    b._.length = 0
+    b._ = []
+    b._host = {
+      av: 0,
+      r: null,
+      ball: null
+    }
   })
   var i, j, len = balls.length
   for (i = 0; i < len; i++) {
@@ -55,6 +54,7 @@ function calc(){
       var d1 = b2.position.clone().sub(b1.position) // b1到b2
       var d2 = d1.clone().negate()
       var r = d1.length()
+      //console.log(r)
       if (r <= b1.R + b2.R) { // 碰撞 标记分量
         b1._.push(d1.clone().normalize())
         b2._.push(d2.clone().normalize())
@@ -66,37 +66,73 @@ function calc(){
       var a2 = F2.clone().divideScalar(b2.m)
       b1.a.add(a1)
       b2.a.add(a2)
+      var av = a1.length()
+      if (av > b1._host.av) {
+        b1._host.av = av
+        b1._host.r = r
+        b1._host.ball = b2
+      }
     }
   }
+
+  var dt = clock.getDelta() // 时间差量
+  //console.log(dt)
+  // 设定dt最大值 解决浏览窗 口切换导致的dt暴增
+  dt = Math.min(dt, 0.1)
+  dt *= 5 // x快进播放
+
+  // todo: 限制位置 不能越过其他物体
   // 根据加速度计算速度/位置
   balls.forEach(function(b){
-    //var _a_ = (b.a.clone().add(b.a0)).divideScalar(2)
-    //b.v.add(_a_.clone().multiplyScalar(dt))
-    b.v.add(b.a.clone().multiplyScalar(dt))
+    var _a_ = (b.a.clone().add(b.a0)).divideScalar(2)
+    b.v.add(_a_.clone().multiplyScalar(dt))
+    //b.v.add(b.a.clone().multiplyScalar(dt))
     b._.forEach(function(d){ // 碰撞 分量清零
       b.v.sub(d.clone().multiplyScalar(b.v.clone().dot(d)))
     })
-    //var _v_ = (b.v.clone().add(b.v0)).divideScalar(2)
-    //b.position.add(_v_.clone().multiplyScalar(dt))
-    b.position.add(b.v.clone().multiplyScalar(dt))
-    //b._v = b.v.clone()
+    var _v_ = (b.v.clone().add(b.v0)).divideScalar(2)
+
+    // 公转匹配
+    if (b._host.av / b.a.length() > 0.9) { // 大作用比
+      var v2a = b.v.angleTo(b.a)
+      if (Math.abs(v2a - Math.PI/2) < Math.PI/2*0.1) { // 约90°夹角
+        var vv = b.v.length()
+        var r = b._host.r
+        var b2 = b._host.ball
+        if (Math.abs(vv - Math.sqrt(G*b2.m/r)) < vv*0.1) { // 约向心力
+          var w = Math.sqrt(G*b2.m/(r*r*r))
+          //
+        }
+      }
+    }
+
+    b.position.add(_v_.clone().multiplyScalar(dt))
+    //b.position.add(b.v.clone().multiplyScalar(dt))
+    b.rotation.x += b.vr.x * dt
+    b.rotation.y += b.vr.y * dt
+    b.rotation.z += b.vr.z * dt
     //console.log('ball[%d]', i, b.position)
   })
 }
 
-function createBall(m, R, p, v, c){
+function createBall(m, R, p, v, vr, c){
   var geometry = new THREE.SphereGeometry( R, 16, 16 );
-  var material = new THREE.MeshBasicMaterial( { color: c } );
+  //var material = new THREE.MeshBasicMaterial( { color: c } );
+  var material = new THREE.MeshBasicMaterial({
+    map: THREE.ImageUtils.loadTexture('textures/'+ c +'.png')
+  })
+
   var ball = new THREE.Mesh( geometry, material );
+  ball.c = c
   ball.position.fromArray(p)
-  ball.m = _M * m
+  ball.m = m
   ball.R = R
   ball.a = new THREE.Vector3()
   ball.v = new THREE.Vector3().fromArray(v)
+  ball.vr = new THREE.Vector3().fromArray(vr)
   //ball.a0 = ball.a.clone()
   //ball.v0 = ball.v.clone()
-  ball._ = []
-  ball.id = '' + Math.random()
+  //ball.id = '' + Math.random()
   return ball;
 }
 
